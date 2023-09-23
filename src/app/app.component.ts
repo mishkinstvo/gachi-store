66f145918903a5bbf8cb3aa78511720ae6e4749a
Mykhailo Skrypkin <mishkinstvo@outlook.com>
2023-09-23 22:03:16 +0300

    Mat table integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Client {
+  id: number;
+  name: string;
+  address: string;
+  debt: number;
+}
+
+const CLIENT_DATA: Client[] = [
+  {id: 1, name: 'Van Darkholme', address: 'Dungeon', debt: 300},
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gachi-store';
+  displayedColumns: string[] = ['id', 'name', 'address', 'debt'];
+  dataSource = CLIENT_DATA;
 }
