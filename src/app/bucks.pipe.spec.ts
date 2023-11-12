dc8b41716a3931373c9556aa6b7e7ee7bff1f228
Mykhailo Skrypkin <mishkinstvo@outlook.com>
2023-11-13 01:24:43 +0200

    Simple bucks pipe

---
+++ src/app/bucks.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { BucksPipe } from './bucks.pipe';
+
+describe('BucksPipe', () => {
+  it('create an instance', () => {
+    const pipe = new BucksPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
